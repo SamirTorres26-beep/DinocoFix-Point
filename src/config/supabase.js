d2cdbddc2013bc0ef901d20c1cea50b9abbb1de6
Samir Torres <222310006@itslerdo.edu.mx>
2026-06-02 05:04:14 -0600

    Configurar SSL para conexión a Supabase

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,12 +1,10 @@
 const { Pool } = require('pg');
-const dns = require('dns');
-
-dns.setDefaultResultOrder('ipv4first');
 
 const pool = new Pool({
     connectionString: process.env.DB_URL,
     ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
+        require: true
     }
 });
 
